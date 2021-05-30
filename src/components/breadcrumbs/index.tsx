2bbe9b11c81464f5bb71bf98d94397d9a69d54c0
Gavin <2829023694@qq.com>
2021-05-30 16:28:24 +0800

    优化面包屑动态显示

--- src/components/breadcrumbs/index.tsx
+++ src/components/breadcrumbs/index.tsx
@@ -4,6 +4,7 @@ import React, {
 import { Breadcrumb } from 'antd';
 import adminPageMenuConfig, { MenuType } from '@src/config/admin-page-menu-config';
 import { useLocation, NavLink } from 'react-router-dom';
+import { deduplication } from '@src/utils/array-deduplication';
 
 interface IProps {
     childrens?:TitlesType[]
@@ -24,23 +25,22 @@ const Breadcrumbs: FC<IProps> = (props:IProps): ReactElement => {
     const recursion = (list:MenuType[]) => list.forEach((item) => {
       if (item.children) {
         item.children.forEach((itemx) => {
-          // if (itemx.routerPath === location.pathname) {
-          //   titles.push({ title: item.title });
-          // }
           if (location.pathname.indexOf((itemx.routerPath as string)) !== -1) {
-            titles.push({ title: item.title });
+            titles.push({ title: item.title, path: item.routerPath });
           }
         });
         recursion(item.children);
       }
-      if (location.pathname === item.routerPath) {
+      if (location.pathname.indexOf((item.routerPath as string)) !== -1) {
+        // console.log(item);
         titles.push({ title: item.title, path: item.routerPath });
       }
     });
     recursion(adminPageMenuConfig);
-    return titles;
+    // 在这里过滤 titles 重复项
+    const removeDuplicationTitleKes = deduplication<TitlesType, 'title'>(titles, 'title');
+    return titles.filter((currentValue: TitlesType, index:number) => currentValue.title === removeDuplicationTitleKes[index]);
   };
-
   return (
     <Breadcrumb>
       { titleList().map((item) => (
--- src/pages/product/c-pages/add-product/index.tsx
+++ src/pages/product/c-pages/add-product/index.tsx
@@ -1,7 +1,16 @@
 import React, { FC, ReactElement, memo } from 'react';
+import Breadcrumbs from '@components/breadcrumbs';
+import { Button, Card } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 
 const AddProduct: FC = (): ReactElement => (
-  <h2>AddProduct</h2>
+  <Card
+    title={<Breadcrumbs />}
+    extra={<Button type="primary" icon={<PlusOutlined />}>添加</Button>}
+    style={{ width: '100%', height: '100%' }}
+  >
+    <h2>DetailProduct</h2>
+  </Card>
 );
 
 export default memo(AddProduct);
